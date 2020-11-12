b6f7dfbf3339d18f97c25c651bf15d65fc0e9fbb
ha49 <kaysersoza@outlook.com>
2020-11-12 23:01:38 +0100

    Custom exception class re committed.

---
+++ src/main/java/se/iths/producerdemo/StudentProducer.java
@@ -0,0 +1,20 @@
+package se.iths.producerdemo;
+
+import se.iths.entity.Student;
+
+import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentProducer {
+    List<Student> listOfStudents = new ArrayList<>();
+
+    @Produces
+    List<Student> getProducedStudents() {
+        listOfStudents.add(new Student("Joe", "May", "aaa", "333333333"));
+        listOfStudents.add(new Student("Joely", "Mayly", "aaa", "333333333"));
+        return listOfStudents;
+    }
+
+
+}
