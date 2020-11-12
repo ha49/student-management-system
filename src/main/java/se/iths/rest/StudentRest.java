795e08dacacfe7825af0aeebc6c92096065ca466
ha49 <kaysersoza@outlook.com>
2020-11-12 23:00:11 +0100

    CRUD operations configured. Custom exception class added

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,83 @@
 package se.iths.rest;
 
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
+
+    @Inject
+    StudentService studentService;
+
+
+    @Path("new")
+    @POST
+    public Response addStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+
+    @Path("update")
+    @PUT
+    public Response updateStudent(Student student) {
+        studentService.updateTodo(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id) {
+        Student foundStudent=studentService.findStudentById(id);
+        if (foundStudent !=null){
+            return Response.ok(foundStudent).build();
+        } else {
+            throw new StudentNotFoundException("Student with ID:" + id+ " not found");
+            //              return Response.status(Response.Status.NOT_FOUND).entity("Item with ID:" + id+ " not found").type(MediaType.TEXT_PLAIN_TYPE).build();
+        }
+    }
+
+
+
+    @Path("{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @DELETE
+
+    public Response removeStudent(@PathParam("id") Long id ){
+        Student foundStudent =studentService.findStudentById(id);
+        if (foundStudent !=null){
+            studentService.deleteStudent(id);
+            return Response.ok().entity("student with id: " + id+ "was successfully removed").build();
+        } else {
+            throw new StudentNotFoundException("Student with ID:" + id+ " not found");
+
+        }
+    }
+
+
+
+    @Path("getall")
+    @GET
+    public List<Student> getAllStudents() {
+        List<Student> studentList=studentService.getAllStudents();
+        if(studentList.isEmpty()){
+            throw new StudentNotFoundException("Currently there is no student information recorded in the database");
+        } else {
+            return studentService.getAllStudents();
+        }
+        }
+
+
+
 
 
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,13 +1,17 @@
 package se.iths.service;
 
+
 import se.iths.entity.Student;
-import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
 
-
+@Transactional
 public class StudentService {
 
-
+    @PersistenceContext
     EntityManager entityManager;
 
     public Student createStudent(Student student) {
@@ -20,6 +24,13 @@ public class StudentService {
         return student;
     }
 
+    public void deleteStudent(Long id){
+        Student deleteThisStudent=entityManager.find(Student.class, id);
+        entityManager.remove(deleteThisStudent);
+
+
+    }
+
     public Student findStudentById(Long id) {
         return entityManager.find(Student.class, id);
     }
@@ -27,5 +38,7 @@ public class StudentService {
     public List<Student> getAllStudents() {
         return entityManager.createQuery("SELECT s from Student s", Student.class).getResultList();
     }
+
+
 
 }
